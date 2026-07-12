0ac24692417ddc661a874e66228e026fbc456f44
rv083 <rakshitvyas2003@gmail.com>
2026-07-12 19:39:38 +0530

    debug

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,8 @@
 import { CheckoutClient } from "@/components/checkout/CheckoutClient";
+import { Suspense } from "react";
 
 export default function CheckoutPage() {
-  return <CheckoutClient />;
-}
+  return <Suspense fallback={<p>Loading...</p>}>
+            <CheckoutClient />
+        </Suspense>
+};
